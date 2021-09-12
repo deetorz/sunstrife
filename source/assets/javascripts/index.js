3670550b41079f88f3a4095820084fcd925f5431
Derek Torres <derek.n.torres@gmail.com>
2021-09-12 19:49:12 +0900

    dropdown functions

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -1,11 +1,9 @@
 import 'bootstrap';
 
-const initDropdown = () => {
-  const icon = document.querySelector("#dropdown");
-  const dropdownContent = document.querySelector(".dropdown-content");
-  if (icon != null) {
-    icon.addEventListener("click", () => {
-      dropdownContent.classList.toggle("show");
-    })
-  }
+const icon = document.querySelector("#dropdown");
+const dropdownContent = document.querySelector(".dropdown-content");
+if (icon != null) {
+  icon.addEventListener("click", () => {
+    dropdownContent.classList.toggle("show");
+  })
 }
